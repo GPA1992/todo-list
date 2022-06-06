d2b218f78a4a9ae412efe52182bbc3583f0d482b
Gabriel Proença de Araujo <gabrielproencaaraujo@gmail.com>
2022-06-06 10:47:24 -0300

    items obrigatórios finalizados

--- script.js
+++ script.js
@@ -10,24 +10,18 @@ function addToDo () {
     const sortTask = document.createElement('li');
     sortTask.className = 'task';
     sortTask.innerText= getTask;
-    sortTask.addEventListener('click', grayPaint)  
-    sortTask.addEventListener('dblclick', completed)  
+    sortTask.addEventListener('click', grayPaint);  
+    sortTask.addEventListener('dblclick', completed); 
     taskList.appendChild(sortTask); 
-
 }
-
-
 
 function clearAll () {
     const clearButton = document.querySelector('#apaga-tudo')
     const taskList = document.querySelector('#lista-tarefas');
     clearButton.addEventListener('click', function () {
     const taskList = document.querySelector('#lista-tarefas').innerHTML = '';
-    })
+})
 }
-
-
-
 
 function grayPaint(event) {
     const task = document.querySelector('.selected');
@@ -38,21 +32,31 @@ function grayPaint(event) {
     task.classList.remove('selected');
     event.target.classList.add('selected');
     }
-    
-  }
-
-function completedTask () {
-  
 }
-
 
 function completed(event) {
   event.target.classList.toggle('completed');
 }
 
+function finishRemove () {
+    const finish = document.querySelectorAll('.completed');
+    for (let i = 0; i < finish.length; i += 1) {
+    finish[i].remove();
+    }
+}
 
+function selectedRemove () {
+    const finish = document.querySelectorAll('.selected');
+    for (let i = 0; i < finish.length; i += 1) {
+    finish[i].remove();
+    }
+}
 
- 
+function storeTask () {
+    const task = document.getElementsByClassName('task');
+    localStorage.setItem('tarefas', task);
+}
+
 
 
 
@@ -60,14 +64,16 @@ function startEvents () {
     document.querySelector('#criar-tarefa').addEventListener('click', function(){
         addToDo ();
         clearInput ();
-        });
-    
-    clearAll();
-    
-    
+    });
+    clearAll();  
+    document.getElementById('remover-finalizados').addEventListener('click', finishRemove)
+    document.getElementById('remover-selecionado').addEventListener('click', selectedRemove)
+    document.getElementById('salvar-tarefas').addEventListener('click', storeTask);
 }
-
 window.addEventListener('load', startEvents);
+
+
+
 
 
 
